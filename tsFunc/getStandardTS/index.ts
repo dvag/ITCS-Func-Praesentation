92a0271494b10edd85060766b06d229984929539
Kurtanovic, Omer <omer.kurtanovic@dvag.com>
2022-11-18 03:03:25 +0100

    Standart und Premium Func zu TypeScript

---
+++ tsFunc/getStandardTS/index.ts
@@ -0,0 +1,31 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = {
+        "title": "Hier kommt ein Titel",
+        "language": "TypeScript",
+        "plan": "Standard Plan",
+        "chapter": "1",
+        "side": "1",
+        "pictureUrl": "https://www.evkircheschaafheim.de/wp-content/uploads/2014/09/Dummybild.jpg",
+        "picturePos": "left",
+        "text": [
+            {
+                "text": "Dies ist Text Nr 1",
+                "type": "main"
+            },
+            {
+                "text": "Dies ist Text Nr 2",
+                "type": "bullet"
+            },
+        ]
+    }
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+};
+
+export default httpTrigger;
