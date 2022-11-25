0e712bcaaf3dc72a9290d9bd0f6d17c6dbdb5dc2
Tobias Kurz <Tobias.Kurz@dvag.com>
2022-11-25 18:54:35 +0100

    HandsOn Function Definition Folie

---
+++ pythonFunc/getHandsOnPython/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import json
+
+import azure.functions as func
+from .data.response import Response
+from .data.text import Text
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    #return func.HttpResponse(f"Dies ist eine Testausgabe von Azure Functions in Python - Standard Plan.")
+    teaser_response = create_teaser_response()
+    return func.HttpResponse(teaser_response.to_json())
+
+
+def create_teaser_response() -> Response:
+    """Erstellen der Teaser-Response
+
+    Returns:
+        Response: Zurückgeben des erstellten Response-Objektes
+    """
+    response = Response()
+    
+    response.title = "Hands On: Function Definition"
+    response.language = "Python 3"
+    response.plan = "Standard Plan"
+    response.chapter = "1"
+    response.side = "1"
+    response.picture_url = "https://www.jeffsblog.at/wp-content/uploads/2019/01/5-Windows-95-wallpaper.jpg"
+    response.picture_pos = "left"
+    response.text = [
+        Text(text="Befehl Func New (Azure Core Tools) wird neue Function angelegt", type="main"), 
+        Text(text="function.json konfiguriert z.B.:", type="main"), 
+        Text(text="Authentifizierung", type="bullet"),
+        Text(text="Routing", type="bullet"), 
+        Text(text="Verfügbare HTTP Methoden", type="bullet"),
+        Text(text="Anbindung an Azure Ressourcen (Tables, Queues, etc.)", type="bullet")
+        ]
+
+    return response
