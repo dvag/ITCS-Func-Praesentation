fc8e97ea8c63f710483103f2e86c81b5e7964243
Tobias Kurz <Tobias.Kurz@dvag.com>
2022-11-25 18:59:58 +0100

    HandsOn: Function Host Folie

--- goFunc/main.go
+++ goFunc/main.go
@@ -90,6 +90,42 @@ func getPremiumGo(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(res)
 }
 
+func getHandsOnGo(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Hit Endpoint getPremiumGo")
+	res := PresResponse{
+		Title:      "Hands On: Function Host",
+		Language:   "Go",
+		Plan:       "Premium",
+		Chapter:    1,
+		Side:       1,
+		PictureUrl: "https://www.evkircheschaafheim.de/wp-content/uploads/2014/09/Dummybild.jpg",
+		PicturePos: "left",
+		Text: []TextPoint{
+			{
+				Text: "In host.json werden Function Parameter definiert:",
+				Type: "main",
+			},
+			{
+				Text: "Name des Binarys",
+				Type: "bullet",
+			},
+			{
+				Text: "Loglevel",
+				Type: "bullet",
+			},
+			{
+				Text: "Health Monitor",
+				Type: "bullet",
+			},
+			{
+				Text: "Azure Erweiterungen",
+				Type: "bullet",
+			}
+		},
+	}
+	json.NewEncoder(w).Encode(res)
+}
+
 func kill(w http.ResponseWriter, r *http.Request) {
 	os.Exit(3)
 }
@@ -98,6 +134,7 @@ func runAPi() {
 	fmt.Printf("Welcome! Available Enpoints are:")
 	fmt.Println("/api/getStandardGo")
 	fmt.Println("/api/getPremiumGo")
+	fmt.Println("/api/getHandsOnGo")
 	fmt.Println("/api/kill")
 	listenAddr := ":8081"
 	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok { // ok will receive a bool that will be set to true if "FUNCTIONS_CUSTOMHANDLER_PORT" was actually present in LookUpEnv
@@ -107,6 +144,7 @@ func runAPi() {
 	srv := http.NewServeMux()
 	srv.HandleFunc("/api/getStandardGo", getStandardGo)
 	srv.HandleFunc("/api/getPremiumGo", getPremiumGo)
+	srv.HandleFunc("/api/getHandsOnGo", getHandsOnGo)
 	srv.HandleFunc("/api/kill", kill)
 	log.Fatal(http.ListenAndServe(listenAddr, srv))
 }
