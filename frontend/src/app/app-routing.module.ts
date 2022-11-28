58754bed2ff4322d1d02f79b47d69efecca62c27
Tobias Kurz <Tobias.Kurz@dvag.com>
2022-11-28 16:01:49 +0100

    DVAG Font

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {KapitelstartComponent} from "./kapitelstart/kapitelstart.component";
-import {TeaserComponent} from "./teaser/teaser.component";
 import {FolieComponent} from "./folie/folie.component";
 import {AgendaComponent} from "./agenda/agenda.component";
 import {AbschlussComponent} from "./abschluss/abschluss.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/kapitel/1/folie/1', pathMatch: 'full'},
-  {path: 'kapitel/1/folie/2', component: TeaserComponent},
   {path: 'kapitel/1/folie/3', component: AgendaComponent},
   {path: 'kapitel/9/folie/1', component: AbschlussComponent},
   {path: 'kapitel/:kapitelId/folie/1', component: KapitelstartComponent},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,7 +8,6 @@ import {AgendaComponent} from './agenda/agenda.component';
 import {KapitelstartComponent} from './kapitelstart/kapitelstart.component';
 import {FolieComponent} from './folie/folie.component';
 import {HttpClientModule} from "@angular/common/http";
-import {TeaserComponent} from "./teaser/teaser.component";
 import {FooterComponent} from './footer/footer.component';
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import { AbschlussComponent } from './abschluss/abschluss.component';
@@ -19,7 +18,6 @@ import { AbschlussComponent } from './abschluss/abschluss.component';
     AgendaComponent,
     KapitelstartComponent,
     FolieComponent,
-    TeaserComponent,
     FooterComponent,
     AbschlussComponent
   ],
